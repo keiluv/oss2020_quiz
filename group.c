e024b920a1cdeacca1baeba81887a4478100cae4
LMK <21800510@handong.edu>
2020-04-13 19:35:16 +0900

    oss_project1

---
+++ group.c
@@ -0,0 +1,40 @@
+#include "group.h"
+
+group* clan[MAX_DATA];
+int gcount =0;
+
+int g_is_available(){
+	int i;
+	for(i=0;i<MAX_DATA;i++)
+	{
+		if(clan[i]==NULL)return 1;
+	}
+	return 0;
+}
+
+int g_first_available(){
+	int i=0;
+	for(i;i<MAX_DATA;i++){
+		if(clan[i]==NULL)return 1;
+	}
+	return -1;
+}
+
+void g_create(char* groupname, int limit_number){
+	int index = g_first_available();
+	clan[index]=(group*)malloc(sizeof(group));
+	group *g = clan[index];
+	strcpy(g->name,groupname);
+	g->group_limit_number = limit_number;
+	gcount++;
+}
+
+group* g_search_by_name(char* groupname){
+	int i=0;
+	for(i;i<MAX_DATA;i++){
+		if(clan[i]!=NULL && strcmp(clan[i]->name, groupname)==0)return clan[i];
+	}
+	return NULL;
+
+
+}
